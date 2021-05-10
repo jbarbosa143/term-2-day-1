adaf64978a006911944a25ca803260c05d9372d2
jeremybarbosa <jbarbosa1430@gmail.com>
2021-05-10 10:40:43 -0500

    brain fart on the last couple func

---
+++ day-1/functions.js
@@ -0,0 +1,87 @@
+const returnEmptyString = function () {
+    return "";
+};
+
+const returnZeroNumber = function () {
+    return 0;
+};
+
+const returnEmptyArray = function () {
+    return [];
+};
+
+const returnEmptyObject = function () {
+    return {};
+};
+
+const returnAbcString = function (value) {
+    return "abc123";
+};
+
+const returnSumOfTen = function (arg1, arg2) {
+    let sum = arg1 + arg2
+    return sum;
+};
+
+const subtraction = function (arg1, arg2) {
+    let sum = arg1 - arg2
+    return sum;
+};
+
+const multiplication = function (arg1, arg2) {
+    let sum = arg1 * arg2
+    return sum;
+};
+
+const division = function (arg1, arg2) {
+    let sum = arg1 / arg2
+    return sum;
+};
+
+const returnArray = function (arg1) {
+    return [1,2,3]
+};
+
+const returnFirstIndex = function (arg1) {
+    return arg1[0];
+};
+
+const returnSecondIndex = function (arg1) {
+    return arg1[1];
+};
+
+const returnArrayLength = function (arg1) {
+    return arg1[2];
+};
+
+const arraySum = function (arg1) {
+    let sum = 0;
+    for(let i = 0; i < arg1.length; i ++){
+        sum += arg1[i]   
+    }
+    return sum;
+};
+
+const arraySubtraction = function (arg1) {
+    let sum = 0;
+    for(let i = 0; i < arg1.length; i ++){
+        sum -= arg1[i]
+    }
+    return sum;
+};
+
+const multiplicationArray = function (arg1) {   
+
+};
+
+const divisionArray = function (arg1) {
+
+};
+
+const oddArray = function (arg1) {
+
+};
+
+const evenArray = function (arg1) {};
+
+const returnFruits = function (arg1) {};
